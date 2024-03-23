813166e8adf1c0cdcd0e2903b18d760da9f6784f
rguevarac85 <rguevarac85@hotmail.com>
2024-03-23 17:20:47 -0500

    correccion de derechos reservados

--- app/Views/layouts/footer.php
+++ app/Views/layouts/footer.php
@@ -3,7 +3,7 @@
     <div class="float-right d-none d-sm-block">
       <b>Version</b> 3.0.2
     </div>
-    <strong>Copyright &copy; 2014-2020 <a href="https://www.facebook.com/Tatope-1629268083750486">Tato.pe</a>.</strong> All rights
+    <strong>Copyright &copy; 2014-2024 <a href="https://www.facebook.com/Tatope-1629268083750486">Tatope.com</a>.</strong> All rights
     reserved.
   </footer>
   <!-- Control Sidebar -->
